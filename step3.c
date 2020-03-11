0473a7a74cf547c78160102b06a9ff1d4d64846e
vPere <46808803+vPere@users.noreply.github.com>
2020-03-11 13:33:20 +0100

    Update step3.c

--- step3.c
+++ step3.c
@@ -16,96 +16,103 @@ int main(int argc, char *argv[]) {
     int pid, ppid;
     int i, top;
     int returnedpid;
-    sem_t*  psem1;
+    sem_t *psem1;
+    sem_t *psem2;
     int  sem_value;
     char filename[64] = "log2.txt";
     int result;
 
-    /* Parse program arguments */
-    /*if (argc != 2) {
-        fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
-        exit(1);
-    }*/
-
     /* Open psem1 */
-    psem1 = (sem_t*) sem_open("/sem1", O_CREAT, 0644, 0);
+    psem1 = (sem_t*) sem_open("/sem3", O_CREAT, 0644, 1);
     if (psem1 == SEM_FAILED) {
         err_sys("Open psem1");
     }
-    sem_getvalue(psem1, &sem_value);
-    while (sem_value > 0) {
-        sem_wait(psem1);
-        sem_value--;
+    psem2 = (sem_t*) sem_open("/sem4", O_CREAT, 0644, 0);
+    if (psem1 == SEM_FAILED) {
+        err_sys("Open psem2");
     }
+	
     /* Create a new process and print */
-   	returnedpid = fork();
-	if(fork()==0){
-		//child process
-		
-		int rep;
-		char msg[255];
-		FILE *toWrite = fopen(filename, "a+");
-		sem_wait(psem1);
-                printf("P1: Enter the number of iterations (0-9): \n");
-                scanf("%d", &rep);
-                if(rep!=0){
-                        printf("P1: Enter the word to be written: \n");
-                        scanf("%s", msg);
-                        strcat(msg, "\n");
-                } else {
-			fclose(toWrite);
-                        sem_destroy(psem1);
-			exit(0);
-                }
-                while(i<rep){
-                         fprintf(toWrite, msg);
-                         i++;
-		}
-		result = sem_getvalue(psem1, &sem_value);
-                if (result < 0) {
-                    err_sys("Read psem1");
-                }
-                printf("PROCESS 1(PSEM1): %d\n", sem_value);
+        returnedpid = fork();
+        if(fork()==0){
+                //child process
 
-		fclose(toWrite);
-		sem_post(psem1);
-	} else  {
-		//parent process
+                int rep;
+                char msg[255];
+                while(1) {
+                        FILE *toWrite = fopen(filename, "a+");
+                        sem_wait(psem1);
+                        sem_getvalue(psem2, &sem_value);
+                        if (sem_value==1){
+                                fclose(toWrite);
+                                sem_destroy(psem1);
+                                sem_destroy(psem2);
+                                exit(0);
+                        }
+                        printf("P1: Enter the number of iterations (0-9): \n");
+                        scanf("%d", &rep);
+                        if(rep!=0){
+                                printf("P1: Enter the word to be written: \n");
+                                scanf("%s", msg);
+                                strcat(msg, "\n");
+                        } else {
+                                fclose(toWrite);
+                                sem_post(psem2);
+                                exit(0);
+                        }
+                        while(i<rep){
+                                 fprintf(toWrite, msg);
+                                 i++;
+                        }
+                        result = sem_getvalue(psem1, &sem_value);
+                        if (result < 0) {
+                            err_sys("Read psem1");
+                        }
+                        printf("PROCESS 1(PSEM1): %d\n", sem_value);
+                        fclose(toWrite);
+                        sem_post(psem2);
+                }
+                } else  {
+                //parent process
                 int rep;
                 char msg[255];
                 FILE *toWrite = fopen(filename, "a+");
-                sem_wait(psem1);
-                printf("P2: Enter the number of iterations (0-9): \n");
-                scanf("%d", &rep);
-                if(rep!=0){
-                        printf("P2: Enter the word to be written: \n");
-                        scanf("%s", msg);
-                        strcat(msg, "\n");
-                } else {
-			fclose(toWrite);
-                        sem_destroy(psem1);
-                        exit(0);
-                }
-                while(i<rep){
-                         fprintf(toWrite, msg);
-                         // Read and print semaphore value
-                        result = sem_getvalue(psem1, &sem_value);
-                        if (result < 0) {
-                                err_sys("Read psem1");
+                while(1){
+                        sem_wait(psem2);
+                        sem_getvalue(psem1, &sem_value);
+                        if (sem_value==1){
+                                fclose(toWrite);
+                                sem_destroy(psem1);
+                                sem_destroy(psem2);
+                                exit(0);
                         }
-                        printf("PROCESS 2(PSEM1): %d\n", sem_value);
-                        i++;
-                }
-		fclose(toWrite);
-                sem_post(psem1);
 
-	}
-    /*
-    top =atoi(argv[1]);
-    for (i=0; i< top;i++) {
-     fprintf(stdout, "value: %d\t",i);
-    }
-    fprintf(stdout, "\n");
-    */
+                        printf("P2: Enter the number of iterations (0-9): \n");
+                        scanf("%d", &rep);
+                        if(rep!=0){
+                                printf("P2: Enter the word to be written: \n");
+                                scanf("%s", msg);
+                                strcat(msg, "\n");
+                        } else {
+                                fclose(toWrite);
+                                sem_destroy(psem2);
+                                sem_post(psem2);
+                                exit(0);
+                                }
+                        while(i<rep){
+                                fprintf(toWrite, msg);
+                                i++;
+                        }
+                        fclose(toWrite);
+                        sem_post(psem1);
+                         result = sem_getvalue(psem1, &sem_value);
+                         if (result < 0) {
+                                 err_sys("Read psem1");
+                         }
+                                 printf("PROCESS 2(PSEM1): %d\n", sem_value);
+
+                }
+        }
+
     exit(0);
 }
